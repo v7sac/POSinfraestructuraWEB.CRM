8bd391011c2584b65abf7baced72a8d9e1b2cafa
v7sac <40270775+v7sac@users.noreply.github.com>
2018-09-12 12:05:55 -0500

    Update __openerp__.py

--- posinfrawebcrm/__openerp__.py
+++ posinfrawebcrm/__openerp__.py
@@ -10,7 +10,7 @@
         Long description of module's purpose
     """,
 
-    'author': "My Company",
+    'author': "SCL-SOFTWEB365",
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
