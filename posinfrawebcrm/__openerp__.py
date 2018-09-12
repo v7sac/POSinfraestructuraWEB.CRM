1acb681180481215823fbafa754e042b5ea5436a
v7sac <40270775+v7sac@users.noreply.github.com>
2018-09-12 12:17:02 -0500

    Update __openerp__.py

--- posinfrawebcrm/__openerp__.py
+++ posinfrawebcrm/__openerp__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "infrasan",
+    'name': "POSinfraestructuraWEB.CRM",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
